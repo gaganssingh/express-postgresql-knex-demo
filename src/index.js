1ec8b88d4054ee3e4be3bc6c0d047fa4e5fea21e
Gagan Singh <gagansingh.dev@outlook.com>
2022-03-27 23:15:21 -0400

    Implemented get by id & delete functionality

--- src/index.js
+++ src/index.js
@@ -14,41 +14,83 @@ app.get("/", (req, res) => res.json({ hello: "world" }));
 // -------------------
 //     channels
 // -------------------
+app
+  .route(`/api/channels`)
+  .get((req, res) => {
+    res.status(200).json(channels);
+  })
+  .post((req, res) => {
+    const channelInfo = req.body;
 
-app.get(`/api/channels`, (req, res) => {
-  res.status(200).json(channels);
-});
+    const newChannel = {
+      id: shortid.generate(),
+      ...channelInfo,
+    };
 
-app.post(`/api/channels`, (req, res) => {
-  const channelInfo = req.body;
+    channels.push(newChannel);
 
-  const newChannel = {
-    id: shortid.generate(),
-    ...channelInfo,
-  };
+    res.status(201).json(newChannel);
+  });
 
-  channels.push(newChannel);
+app
+  .route(`/api/channels/:id`)
+  .get((req, res) => {
+    const { id } = req.params;
+    const channel = channels.find((ch) => ch.id === id);
+    if (!channel) {
+      return res.status(404).json({ error: "Channel not found" });
+    }
+    res.status(200).json(channel);
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+    const channelIndex = channels.findIndex((ch) => ch.id === id);
+    if (channelIndex === -1) {
+      return res.status(404).json({ error: "Channel not found" });
+    }
 
-  res.status(201).json(newChannel);
-});
+    channels.splice(channelIndex, 1);
+    res.status(204).end();
+  });
 
 // -------------------
 //     lessons
 // -------------------
-app.get(`/api/lessons`, (req, res) => {
-  res.status(200).json(lessons);
-});
+app
+  .route(`/api/lessons`)
+  .get((req, res) => {
+    res.status(200).json(lessons);
+  })
+  .post((req, res) => {
+    const lessonInfo = req.body;
 
-app.post(`/api/lessons`, (req, res) => {
-  const lessonInfo = req.body;
+    const newLesson = {
+      id: shortid.generate(),
+      ...lessonInfo,
+    };
+    lessons.push(newLesson);
+    res.status(201).json(newLesson);
+  });
 
-  const newLesson = {
-    id: shortid.generate(),
-    ...lessonInfo,
-  };
-  lessons.push(newLesson);
-  res.status(201).json(newLesson);
-});
+app
+  .route(`/api/lessons/:id`)
+  .get((req, res) => {
+    const { id } = req.params;
+    const lesson = lessons.find((lsn) => lsn.id === id);
+    if (!lesson) {
+      return res.status(404).json({ error: "Lesson not found" });
+    }
+    res.status(200).json(lesson);
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+    const lessonIndex = lessons.findIndex((lsn) => lsn.id === id);
+    if (lessonIndex === -1) {
+      return res.status(404).json({ error: "Lesson not found" });
+    }
+    lessons.splice(lessonIndex, 1);
+    res.status(204).end();
+  });
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`✅ [SERVER] Listening on port: ${PORT}`));
