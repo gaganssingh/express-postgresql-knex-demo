11414de28f2b7cb5f6334b86794d9152611252b3
Gagan Singh <gagansingh.dev@outlook.com>
2022-03-28 21:16:34 -0400

    Added postgresql config for production

--- knexfile.js
+++ knexfile.js
@@ -20,4 +20,15 @@ module.exports = {
       },
     },
   },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+  },
 };
