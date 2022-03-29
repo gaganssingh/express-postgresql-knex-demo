9bbefdaa3dbba47c446467c2b45cadc4323d1d23
Gagan Singh <gagansingh.dev@outlook.com>
2022-03-28 23:16:01 -0400

    updated ssl config flag

--- knexfile.js
+++ knexfile.js
@@ -22,8 +22,10 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized },
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
     pool: {
       min: 2,
       max: 10,
