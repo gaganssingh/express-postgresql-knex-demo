0d967910fb4ac5d0f1d7a0d5afc8f1b02bf9094e
Gagan Singh <gagansingh.dev@outlook.com>
2022-03-28 21:25:05 -0400

    added basic error handling using try-catch

--- src/routes/lessons.router.js
+++ src/routes/lessons.router.js
@@ -4,11 +4,17 @@ const MessagesService = require("../services/messages.service");
 const knex = require("../db/connection");
 const lessonsRouter = express.Router();
 
+// api/lessons
 lessonsRouter
   .route(`/`)
   .get(async (req, res) => {
-    const response = await LessonsService.find(knex);
-    res.status(200).json(response);
+    try {
+      const response = await LessonsService.find(knex);
+      res.status(200).json(response);
+    } catch (error) {
+      console.error(error);
+      res.status(500).end();
+    }
   })
   .post((req, res) => {
     LessonsService.add(knex, req.body)
@@ -16,66 +22,104 @@ lessonsRouter
       .catch((error) => res.status(500).json({ message: "cannot add lesson" }));
   });
 
+// api/lessons/:id
 lessonsRouter
   .route(`/:id`)
   .get(async (req, res) => {
-    const response = await LessonsService.findById(knex, req.params.id);
-    if (!response) {
-      return res.status(404).json({ error: "lesson not found" });
+    try {
+      const response = await LessonsService.findById(knex, req.params.id);
+      if (!response) {
+        return res.status(404).json({ error: "lesson not found" });
+      }
+      res.status(200).json(response);
+    } catch (error) {
+      console.error(error);
+      res.status(500).end();
     }
-    res.status(200).json(response);
   })
   .delete(async (req, res) => {
-    const response = await LessonsService.remove(knex, req.params.id);
-    if (!response) {
-      return res.status(404).json({ error: "lesson not found" });
+    try {
+      const response = await LessonsService.remove(knex, req.params.id);
+      if (!response) {
+        return res.status(404).json({ error: "lesson not found" });
+      }
+      res.status(200).end();
+    } catch (error) {
+      console.error(error);
+      res.status(500).end();
     }
-    res.status(200).end();
   })
   .patch(async (req, res) => {
-    const response = await LessonsService.update(knex, req.params.id, req.body);
-    if (!response) {
-      return res.status(404).json({ error: "lesson not found" });
+    try {
+      const response = await LessonsService.update(
+        knex,
+        req.params.id,
+        req.body
+      );
+      if (!response) {
+        return res.status(404).json({ error: "lesson not found" });
+      }
+      res.status(200).json(response);
+    } catch (error) {
+      console.error(error);
+      res.status(500).end();
     }
-    res.status(200).json(response);
   });
 
 // ----------------------------------
 //        messages
 // ----------------------------------
+// api/lessons/:id/messages
 lessonsRouter
   .route(`/:id/messages`)
   .get(async (req, res) => {
     const { id: lesson_id } = req.params;
-    const messages = await MessagesService.findByLessonId(knex, lesson_id);
-    res.status(200).json(messages);
+
+    try {
+      const messages = await MessagesService.findByLessonId(knex, lesson_id);
+      res.status(200).json(messages);
+    } catch (error) {
+      console.error(error);
+      res.status(500).end();
+    }
   })
   .post(async (req, res) => {
     const { sender, text } = req.body;
     const { id: lesson_id } = req.params;
 
-    const findLesson = await LessonsService.findById(knex, lesson_id);
-    if (!findLesson) {
-      return res.status(404).json({ error: "lesson not found" });
-    }
+    try {
+      const findLesson = await LessonsService.findById(knex, lesson_id);
+      if (!findLesson) {
+        return res.status(404).json({ error: "lesson not found" });
+      }
 
-    const newMessage = {
-      sender,
-      text,
-      lesson_id,
-    };
-    const message = await MessagesService.addMessage(knex, newMessage);
-    res.status(201).json(message);
+      const newMessage = {
+        sender,
+        text,
+        lesson_id,
+      };
+      const message = await MessagesService.addMessage(knex, newMessage);
+      res.status(201).json(message);
+    } catch (error) {
+      console.error(error);
+      res.status(500).end();
+    }
   });
 
+// api/lessons/:lesson_id/messages/:id
 lessonsRouter
   .route(`/:lesson_id/messages/:id`)
   .delete(async (req, res) => {
-    const response = await MessagesService.removeMessage(knex, req.params.id);
-    if (!response) {
-      return res.status(404).json({ error: "message not found" });
+    try {
+      const response = await MessagesService.removeMessage(knex, req.params.id);
+      if (!response) {
+        return res.status(404).json({ error: "message not found" });
+      }
+      res.status(204).end();
+    } catch (error) {
+      console.error(error);
+      res.status(500).end();
     }
-    res.status(204).end();
   })
   .patch(async (req, res) => {});
 
