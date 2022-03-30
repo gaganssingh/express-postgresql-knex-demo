273642b4cc0ae1044f63607f5e3cfc797b81a235
Gagan Singh <gagansingh.dev@outlook.com>
2022-03-30 16:07:43 -0400

    Refactored reister/login endpoints into a separate auth route. Implemented basic cookie based auth flow.

--- src/app.js
+++ src/app.js
@@ -1,17 +1,36 @@
 const express = require("express");
+const session = require("express-session");
+const requireAuth = require("./middlewares/require-auth");
+const authRouter = require("./routes/auth.router");
 const lessonsRouter = require("./routes/lessons.router");
 const usersRouter = require("./routes/users.router");
+
+// INITIALIZE APP
 const app = express();
+
+const sessionConfig = {
+  name: "monster",
+  secret: process.env.COOKIE_SESSION_SECRET,
+  cookie: {
+    maxAge: 1000 * 60 * 60, // cookie valid for 1 hour
+    secure: false, // For production -> set to https only
+    httpOnly: true, // access only from html
+  },
+  resave: false,
+  saveUninitialized: true, // false in prod to make it GDPR compliant
+};
 
 // MIDDLEWARES
 app.use(express.json());
+app.use(session(sessionConfig));
 
 // ROUTES
 app.get("/", (req, res) => {
   res.json({ message: "Hello from the server!" });
 });
 
-app.use(`/api/lessons`, lessonsRouter);
-app.use(`/api/users`, usersRouter);
+app.use(`/api/auth`, authRouter);
+app.use(`/api/lessons`, requireAuth, lessonsRouter);
+app.use(`/api/users`, requireAuth, usersRouter);
 
 module.exports = app;
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,73 +1,13 @@
 const express = require("express");
 const knex = require("../db/connection");
 const UsersService = require("../services/users.service");
-const bcrypt = require("bcryptjs");
 
 const usersRouter = express.Router();
 
-usersRouter
-  .route(`/register`)
-  .get(async (req, res) => {
-    try {
-      const users = await UsersService.findAllUsers(knex);
-      res.status(200).json(users);
-    } catch (error) {
-      console.error(error);
-      res.status(500).end();
-    }
-  })
-  .post(async (req, res) => {
-    const { username, password } = req.body;
-    if (!username || !username.length) {
-      return res.status(400).json({ error: "username field is required" });
-    }
-    if (!password || !password.length) {
-      return res.status(400).json({ error: "password field is required" });
-    }
-    if (password.length < 6 || password.length > 35) {
-      return res
-        .status(400)
-        .json({ error: "password must be between 6 and 35 characters" });
-    }
-
-    try {
-      // HASH PASSWORD BEFORE SAVING TO DB
-      const hashedPassword = bcrypt.hashSync(password, 12);
-
-      const newUser = { username, password: hashedPassword };
-      const user = await UsersService.addUser(knex, newUser);
-      res.status(201).json(user);
-    } catch (error) {
-      console.error(error);
-      res.status(500).end();
-    }
-  });
-
-usersRouter.route(`/login`).post(async (req, res) => {
-  const { username, password } = req.body;
-  if (!username || !username.length) {
-    return res.status(400).json({ error: "username field is required" });
-  }
-  if (!password || !password.length) {
-    return res.status(400).json({ error: "password field is required" });
-  }
-
+usersRouter.route(`/`).get(async (req, res) => {
   try {
-    const user = await UsersService.findUserByUsername(knex, username);
-    if (!user) {
-      return res.status(404).json({ error: "user doesn't exist" });
-    }
-
-    const comparePassword = bcrypt.compareSync(password, user.password);
-
-    if (!comparePassword) {
-      return res.status(401).json({ error: "unauthorized access" });
-    }
-
-    return res.status(200).json({
-      message: "successfully logged in",
-      user: user.username,
-    });
+    const users = await UsersService.findAllUsers(knex);
+    res.status(200).json(users);
   } catch (error) {
     console.error(error);
     res.status(500).end();
